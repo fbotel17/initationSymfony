682a6b1226d35d985f845a97204f84bfef46d85f
fbotel17 <faustbtl17@gmail.com>
2023-05-22 09:02:11 +0200

    first commit

---
+++ src/Controller/BonjourController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class BonjourController
+{
+
+    function bonjour()
+    {
+        return new Response("Bonjour tout le monde");
+    }
+}
---
+++ src/Entity/Chauffeur.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ChauffeurRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ChauffeurRepository::class)]
+class Chauffeur
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Matricule = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMatricule(): ?string
+    {
+        return $this->Matricule;
+    }
+
+    public function setMatricule(string $Matricule): self
+    {
+        $this->Matricule = $Matricule;
+
+        return $this;
+    }
+}
