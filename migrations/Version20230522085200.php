c162a8f5a0adf3d1f9592a3c3c67ae35ce2c50ad
fbotel17 <faustbtl17@gmail.com>
2023-05-23 08:56:53 +0200

    création page depot

--- migrations/Version20230522085200.php
+++ migrations/Version20230522085200.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230517132539 extends AbstractMigration
+final class Version20230522085200 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,17 @@ final class Version20230517132539 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE ingredient (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(50) NOT NULL, price DOUBLE PRECISION NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE recipe_ingredient DROP FOREIGN KEY FK_22D1FE1359D8A214');
+        $this->addSql('ALTER TABLE recipe_ingredient DROP FOREIGN KEY FK_22D1FE13933FE08C');
+        $this->addSql('DROP TABLE chauffeur');
         $this->addSql('DROP TABLE ingredient');
+        $this->addSql('DROP TABLE recipe');
+        $this->addSql('DROP TABLE recipe_ingredient');
+        $this->addSql('DROP TABLE messenger_messages');
     }
 }
